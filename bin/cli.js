ae0763612678883ed3d41f613457b59b53ddee51
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2019-05-27 07:43:32 +0200

    set default of stop date to start date

--- bin/cli.js
+++ bin/cli.js
@@ -8,73 +8,82 @@
 const inquirer = require('inquirer');
 const fs = require('fs-extra');
 const path = require('path');
-const kgPath = path.resolve('.', 'kg');
 const validator = require('validator');
 const generate = require('../index');
 
 console.log(fs.readFileSync(path.resolve(__dirname, '../intro.txt'), 'utf-8'));
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'Event name',
-      default: 'My Acadamic Event'
-    },
-    {
-      type: 'input',
-      name: 'baseurl',
-      message: 'Base URL',
-      default: 'http://example.com/',
-      validate: (data) => {
-        return validator.isURL(data) ? true : 'Please provide a valid URL.';
+start();
+
+async function start() {
+  const answers = await inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Event name',
+        default: 'My Acadamic Event'
+      },
+      {
+        type: 'input',
+        name: 'baseurl',
+        message: 'Base URL',
+        default: 'http://example.com/',
+        validate: (data) => {
+          return validator.isURL(data) ? true : 'Please provide a valid URL.';
+        }
+      },
+      {
+        type: 'input',
+        name: 'organizers',
+        message: 'Organizers (name <email> (website))'
+      },
+      {
+        type: 'input',
+        name: 'topics',
+        message: 'Topics'
+      },
+      {
+        type: 'input',
+        name: 'twitter',
+        message: 'Twitter'
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Email',
+        validate: (data) => {
+          return (validator.isEmail(data) || data === '') ? true : 'Please provide a valid email or leave empty.';
+        }
+      },
+      {
+        type: 'input',
+        name: 'startdate',
+        message: 'Start date'
       }
-    },
-    {
-      type: 'input',
-      name: 'organizers',
-      message: 'Organizers (name <email> (website))'
-    },
-    {
-      type: 'input',
-      name: 'topics',
-      message: 'Topics'
-    },
-    {
-      type: 'input',
-      name: 'twitter',
-      message: 'Twitter'
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'Email',
-      validate: (data) => {
-        return (validator.isEmail(data) || data === '')? true : 'Please provide a valid email or leave empty.';
-      }
-    },
-    {
-      type: 'input',
-      name: 'startdate',
-      message: 'Start date'
-    },
-    {
-      type: 'input',
-      name: 'enddate',
-      message: 'End date'
-    },
-    {
-      type: 'input',
-      name: 'location',
-      message: 'Location'
-    },
-    {
-      type: 'input',
-      name: 'superevent',
-      message: 'Super event'
-    }
-  ])
-  .then((answers) => {
-    generate(answers, process.cwd());
-  });
+    ]);
+
+  const answers2 = await inquirer.prompt([{
+    type: 'input',
+    name: 'enddate',
+    message: 'End date',
+    default: answers.startdate === '' ? undefined : answers.startdate
+  },
+  {
+    type: 'input',
+    name: 'location',
+    message: 'Location'
+  },
+  {
+    type: 'input',
+    name: 'superevent',
+    message: 'Super event'
+  }]);
+
+  generate(extend(answers, answers2), process.cwd());
+}
+
+function extend(obj, src) {
+  Object.keys(src).forEach(function(key) { obj[key] = src[key]; });
+  return obj;
+}
